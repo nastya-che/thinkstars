640b78e44ebc44f4b144c3fce99c49726a46a371
nChe <nasstya.chernyshova@gmail.com>
2019-12-29 20:06:02 +0200

    Updates

---
+++ precache-manifest.2b17a57b185b5b24798fef06fc001d51.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b667f1bc336d3e30dd5e5d841be8f167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ba008e7837b78fdc5c",
+    "url": "/static/css/main.e8062183.chunk.css"
+  },
+  {
+    "revision": "ac5fb742839adfe9eeb4",
+    "url": "/static/js/2.a40ec488.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.a40ec488.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14ba008e7837b78fdc5c",
+    "url": "/static/js/main.22efd50e.chunk.js"
+  },
+  {
+    "revision": "b9862a793e1ce174a401",
+    "url": "/static/js/runtime-main.c42111ca.js"
+  },
+  {
+    "revision": "b4cca84903cf902fa62a9d7cd348c775",
+    "url": "/static/media/annie-spratt-608001-unsplash-1024x576.b4cca849.jpg"
+  },
+  {
+    "revision": "57c1869d04a5b51e16eb250f560a6a27",
+    "url": "/static/media/favicon120.57c1869d.png"
+  },
+  {
+    "revision": "71f8efa83ffbfd440ed7a71b0ea6fd92",
+    "url": "/static/media/footer2.71f8efa8.jpg"
+  },
+  {
+    "revision": "caa67de6bc9660ace49b53efc5a8399b",
+    "url": "/static/media/logo.caa67de6.svg"
+  },
+  {
+    "revision": "1238339f32433dc75b8883026c80552f",
+    "url": "/static/media/office.1238339f.jpg"
+  }
+]);
